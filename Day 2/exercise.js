b5e5eb8289de4e9650f16558414e4f58332cfaf2
Sudhanshu Mukherjee <sudhanshumukherjeexx@gmail.com>
2022-06-22 16:42:43 +0530

    Day 2
    Adding exercises

---
+++ Day 2/exercise.js
@@ -0,0 +1,32 @@
+//Exercise - Level 1 - 25 Questions
+
+let challenge = "30 Days of JavaScript"
+console.log(challenge)
+console.log(challenge.length)
+console.log(challenge.toUpperCase())
+console.log(challenge.toLowerCase())
+console.log(challenge.substring(8,))
+console.log(challenge.substring(3,))
+console.log(challenge.includes('Script'))
+console.log(challenge.split())
+console.log(challenge.split(' '))
+let exercise_10 = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+console.log(exercise_10.split(','))
+console.log(challenge.replace('JavaScript','Python'))
+console.log(challenge.charAt(15))
+console.log(challenge.charCodeAt(12))
+console.log(challenge.indexOf('a'))
+console.log(challenge.lastIndexOf('a'))
+let exercise_17 = "You cannot end a sentence with because because is a conjunction."
+console.log(exercise_17.indexOf('because'))
+console.log(exercise_17.lastIndexOf('because'))
+console.log(exercise_17.search(/because/gi))
+let exercise_20 = "        30 Days of JavaScript          "
+console.log(exercise_20.trim())
+console.log(challenge.startsWith(30))
+console.log(challenge.endsWith('t'))
+console.log(challenge.match('a'))
+let exercise_24a = '30 Days of '
+let exercise_24b = 'JavaScript'
+console.log(exercise_24a.concat(exercise_24b))
+console.log(challenge.repeat(2))
