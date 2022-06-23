06bdef56e7dc43a532e11a60b2b2a4114ddcac54
Sudhanshu Mukherjee <sudhanshumukherjeexx@gmail.com>
2022-06-23 14:41:25 +0530

    Update exercise.js
    level 2 exercise

--- Day 2/exercise.js
+++ Day 2/exercise.js
@@ -30,3 +30,44 @@ let exercise_24a = '30 Days of '
 let exercise_24b = 'JavaScript'
 console.log(exercise_24a.concat(exercise_24b))
 console.log(challenge.repeat(2))
+
+// Exercise : Level 2
+console.log("Exercise - Level 2")
+
+console.log("The quote \'There is no exercise better for the heart than reaching down and lifting people up.\' by John Holmes teaches us to \'help\' one another.")
+
+console.log("\"LOve is not patronizing and charity isn\'t about pity. it is about love, so don\'t just give money but reach out your hand instead\"")
+
+let ex_3 = '10'
+let ex_31 = Number(ex_3)
+console.log(typeof(ex_31))
+
+console.log(Math.ceil(parseFloat('9.8')))
+
+let ex_5 = 'Python'
+let ex_51 = 'Jargon'
+console.log(ex_5.includes('on'))
+console.log(ex_51.includes('on'))
+
+const ex_num = Math.random()
+let num_bet_0_and_100 = ex_num * 101
+console.log(Math.floor(num_bet_0_and_100))
+
+const ex_8 = Math.floor(Math.random() + 50 * 1.5)
+console.log(ex_8)
+
+const ex_9 = Math.floor(Math.random() * 256)
+console.log(ex_9)
+
+let ex_10 = "JavaScript"
+console.log(ex_10[0])
+
+
+console.log("\n 1 1 1 1 1 \n 2 1 2 4 8 \n 3 1 3 9 27 \n 4 1 4 16 64 \n 5 1 5 25 125")
+
+let ex_12 = "You cannot end a sentence with because because because is a conjunction."
+console.log(ex_12.indexOf('because'))
+console.log(ex_12.lastIndexOf('because'))
+console.log(ex_12.substr(31, 23))
+
+
